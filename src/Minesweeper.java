1b442b37246a153c3fe7554d43c9b9aa7892d704
zezmo <zezmo.o7@gmail.com>
2025-07-18 17:41:00 -0400

    scanning for mines

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -18,7 +18,7 @@ public class Minesweeper {
 
         setUpBoard();
         placeMines(mines);
-
+        scanForMines();
     }
 
     private void setUpBoard() {
@@ -34,12 +34,63 @@ public class Minesweeper {
         int randomColumn, randomRow;
         Random random = new Random();
 
-        for(int i = 0; i < boardMines; i++) {
+        for (int i = 0; i < boardMines; i++) {
             randomColumn = random.nextInt(boardColumns);
             randomRow = random.nextInt(boardRows);
 
             board[randomColumn][randomRow].placeMine();
 
+        }
+    }
+
+    private void scanForMines () {
+        /*
+        *    ex:
+        *        [*][1][0][0]
+        *        [1][2][1][1]
+        *        [0][0][0][*]
+        *
+        *    checking for cell x at [i][j]
+        *         -1  0  1
+        *       -1[ ][ ][ ]
+        *        0[ ][x][ ]
+        *        1[ ][ ][ ]
+        *
+        *    // first scanning method that came to mind might want to make this more efficient somehow
+        */ 
+        for (int i = 0; i < boardColumns; i++) {
+            for (int j = 0; j < boardRows; j++) {
+                //skip the scan if current cell has a mine
+                if( !board[i][j].cellMine ) {
+                    //check row above
+                    if ( board[i-1][j-1].cellMine) {
+                        board[i][j].cellMinesNearby++;
+                    }
+                    if ( board[i][j-1].cellMine) {
+                        board[i][j].cellMinesNearby++;
+                    }
+                    if ( board[i+1][j-1].cellMine) {
+                        board[i][j].cellMinesNearby++;
+                    }
+                    //check each side
+                    if ( board[i-1][j].cellMine) {
+                        board[i][j].cellMinesNearby++;
+                    }
+                    if ( board[i+1][j].cellMine) {
+                        board[i][j].cellMinesNearby++;
+                    }
+                    //check row below
+                    if ( board[i-1][j+1].cellMine) {
+                        board[i][j].cellMinesNearby++;
+                    }
+                    if ( board[i][j+1].cellMine) {
+                        board[i][j].cellMinesNearby++;
+                    }
+                    if ( board[i+1][j+1].cellMine) {
+                        board[i][j].cellMinesNearby++;
+                    }
+                }
+            }
         }
     }
 }
