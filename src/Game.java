608922c26a9a5e5a77dc59b3b0c538798ab788e4
zezmo <zezmo.o7@gmail.com>
2025-07-18 19:01:50 -0400

    setting up basic window elements

---
+++ src/Game.java
@@ -0,0 +1,49 @@
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.event.*;
+
+
+public class Game extends Frame implements WindowListener{    
+
+    public Game () {
+        setLayout(new GridLayout());
+        
+        Panel pnl = new Panel();
+
+        add(pnl);
+        addWindowListener(this);
+
+        setTitle("Minesweeper");
+        setSize(300, 300);
+        setVisible(true);
+        
+    }
+
+    public void windowClosed(WindowEvent e) {
+
+    }
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+    public void windowOpened(WindowEvent e) {
+    }
+
+    public void windowIconified(WindowEvent e) {
+    }
+
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    public void windowActivated(WindowEvent e) {
+    }
+
+    public void windowDeactivated(WindowEvent e) {
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
+
+
+}
